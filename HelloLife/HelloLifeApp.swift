39bdface9edf16f41d5d5a588e9ba9490cba0a12
Mulili Nzuki <mulili.nzuki@gmail.com>
2024-07-13 20:29:53 +0300

    init commit

---
+++ HelloLife/HelloLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloLifeApp.swift
+//  HelloLife
+//
+//  Created by Mulili Nzuki on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloLifeUITests/HelloLifeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HelloLifeUITestsLaunchTests.swift
+//  HelloLifeUITests
+//
+//  Created by Mulili Nzuki on 13/07/2024.
+//
+
+import XCTest
+
+final class HelloLifeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
